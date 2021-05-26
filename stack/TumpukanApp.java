0af9b76278bc3a2a40daa226b937b5e08c4a3cc7
TegarRizky123 <72007606+TegarRizky123@users.noreply.github.com>
2021-05-26 10:16:41 +0700

    Add files via upload

---
+++ stack/TumpukanApp.java
@@ -0,0 +1,20 @@
+package stack;
+public class TumpukanApp {
+    public static void main (String [] args){
+        contoh tumpukan = new contoh(10);
+        tumpukan.push(50);
+        tumpukan.printData();
+        tumpukan.push(100);
+        tumpukan.printData();
+        tumpukan.pop();
+        tumpukan.printData();
+        tumpukan.push(60);
+        tumpukan.printData();
+        tumpukan.push(80);
+        tumpukan.printData();
+        tumpukan.pop();
+        tumpukan.printData();
+        long bacaTop = tumpukan.peek();
+        System.out.println("nilai top = "+bacaTop);
+    }
+}
